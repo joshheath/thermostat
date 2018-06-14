9b6680f279cc22d08d511f694bac97a4a24ab317
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-14 18:00:56 +0100

    starting temp, increase, decrease, PSM, minimum temp

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('Thermostat', function(){
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases in temperature with up()', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in termperature with down()', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+  for (var i = 0; i < 11; i++) {
+    thermostat.down();
+  }
+  expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function(){
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+});
