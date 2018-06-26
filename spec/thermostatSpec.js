98e292557e524a69de28a2ce7d49c188a7a5f4fb
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-26 10:26:10 +0100

    add reset temperature functionality

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -62,4 +62,12 @@ describe('Thermostat', function(){
       expect(thermostat.getCurrentTemperature()).toEqual(32);
     });
   });
+
+  it('can be reset to default temperature', function(){
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemperature();
+    expect(thermostat.getCurrentTemperature).toEqual(20);
+  });
 });
