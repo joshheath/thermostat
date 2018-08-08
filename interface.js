d798b2a01ee3c3bf8a6b3a29f9101e96dcbfbe75
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-08-08 12:47:06 +0100

    adding in current local temperature funtionality

--- interface.js
+++ interface.js
@@ -1,18 +1,10 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
-
-//API to get current weather in London
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  $('#current-temperature').text(data.main.temp);
+    $('#current-temperature').text(data.main.temp);
   })
 
-  displayWeather('London');
-  $('#select-city').submit(function(event) {
-    event.preventDefault();
-    var city = $('#current-city').val();
-    displayWeather(city);
-  });
 
   $('#temperature-up').click(function(){
     thermostat.up();
